831614d9e7157b193a00f471d1541e5ec6e15d23
A91y <65825207+A91y@users.noreply.github.com>
2024-08-13 00:08:26 +0530

    feat: completed vote-program

---
+++ vote-program/programs/vote-program/src/lib.rs
@@ -0,0 +1,89 @@
+use anchor_lang::prelude::*;
+
+declare_id!("DWZLW6oprmJsbjeE1H5dzNmmk7y2rQHY2UHaKpyVhixc");
+
+#[program]
+pub mod vote_program {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, _url: String) -> Result<()> {
+        ctx.accounts.initialize(&ctx.bumps)?;
+        
+        Ok(())
+    }
+
+    pub fn upvote(ctx: Context<Vote>, _url: String) -> Result<()> {
+        ctx.accounts.upvote(ctx.accounts.signer.key())?;
+
+        Ok(())
+    }
+
+    pub fn downvote(ctx: Context<Vote>, _url: String) -> Result<()> {
+        ctx.accounts.downvote(ctx.accounts.signer.key())?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(_url: String)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        init,
+        payer = payer,
+        seeds = [_url.as_bytes().as_ref()],
+        bump,
+        space = VoteState::INIT_SPACE
+    )]
+    pub vote_account: Account<'info, VoteState>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.vote_account.score = 0;
+        self.vote_account.bump = bumps.vote_account;
+        self.vote_account.last_vote = None;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(_url: String)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [_url.as_bytes().as_ref()],
+        bump = vote_account.bump,
+    )]
+    pub vote_account: Account<'info, VoteState>,
+}
+
+impl<'info> Vote<'info> {
+    pub fn upvote(&mut self, signer_key: Pubkey) -> Result<()> {
+        self.vote_account.score += 1;
+        self.vote_account.last_vote = Some(signer_key);
+        Ok(())
+    }
+
+    pub fn downvote(&mut self, signer_key: Pubkey) -> Result<()> {
+        self.vote_account.score -= 1;
+        self.vote_account.last_vote = Some(signer_key);
+        Ok(())
+    }
+}
+
+#[account]
+pub struct VoteState {
+    pub score: i64,
+    pub bump: u8,
+    pub last_vote: Option<Pubkey>,
+}
+
+impl Space for VoteState {
+    const INIT_SPACE: usize = 8 + 8 + 1 + 33;
+}
