5143a2ce641eb76a6f4f6405e4578739027751ad
A91y <65825207+A91y@users.noreply.github.com>
2024-08-13 19:57:15 +0530

    feat: add close method to vault

--- anchor_vault/programs/anchor_vault/src/lib.rs
+++ anchor_vault/programs/anchor_vault/src/lib.rs
@@ -23,6 +23,11 @@ pub mod anchor_vault {
         ctx.accounts.withdraw(amount)?;
         Ok(())
     }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -107,6 +112,53 @@ impl<'info> Payments<'info> {
         let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
 
         transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"vault", state.key().as_ref()],
+        bump = state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"state", user.key().as_ref()],
+        bump = state.state_bump,
+        close = user,
+    )]
+    pub state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let seeds = &[
+            b"vault",
+            self.state.to_account_info().key.as_ref(),
+            &[self.state.vault_bump],
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let balance = self.vault.to_account_info().get_lamports();
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, balance)?;
 
         Ok(())
     }
